ddb4af3ad0f0a9d791fa13e5db79bfa74fa77310
SirVeggie <sirveggie@hotmail.com>
2021-05-25 12:29:33 +0300

    1a

--- part-1/app-ts/src/App.tsx
+++ part-1/app-ts/src/App.tsx
@@ -1,29 +1,58 @@
 import React from 'react';
 
 function App() {
-  const now = new Date();
+  const course = 'Half stack application development';
+  const part1 = 'Fundamentals of React';
+  const exercises1 = 10;
+  const part2 = 'Using props to pass data';
+  const exercises2 = 7;
+  const part3 = 'State of a component';
+  const exercises3 = 14;
+
+  const details = [
+    { description: part1, points: exercises1 },
+    { description: part2, points: exercises2 },
+    { description: part3, points: exercises3 }
+  ];
+
+  const total = {
+    description: 'Number of exercises',
+    points: exercises1 + exercises2 + exercises3
+  };
 
   return (
     <div>
-      <p>Hello world, it is {now.toString()}</p>
-      <ShowPerson name='Jack' age={20} sex='male' />
-      <ShowPerson name='Janette' age={21} sex='female' />
+      <Header data={course} />
+      <Content data={details} />
+      <Total data={total} />
     </div>
   );
 }
 
-function ShowPerson(props: Person) {
+function Header(props: Prop<string>) {
+  return (<h1>{props.data}</h1>);
+}
+
+function Content(props: Prop<Detail[]>) {
   return (
     <div>
-      <p>Person {props.name} is of age {props.age} and is a {props.sex}</p>
+      <Part data={props.data[0]} />
+      <Part data={props.data[1]} />
+      <Part data={props.data[2]} />
     </div>
   );
 }
 
-interface Person {
-  name: string;
-  age: number;
-  sex: string;
+function Total(props: Prop<Detail>) {
+  return (
+    <Part data={props.data} />
+  );
+}
+
+function Part(props: Prop<Detail>) {
+  return (
+    <p>{props.data.description}: {props.data.points}</p>
+  );
 }
 
 export default App;
--- part-1/app-ts/src/react-app-env.d.ts
+++ part-1/app-ts/src/react-app-env.d.ts
@@ -1,1 +1,10 @@
 /// <reference types="react-scripts" />
+
+interface Detail {
+	description: string;
+	points: number;
+}
+
+interface Prop<Type> {
+	data: Type;
+}
